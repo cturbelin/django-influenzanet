3d8aea99446a9a7c747efa15a98d7b3913356cda
Clément Turbelin <clement.turbelin@upmc.fr>
2020-03-13 14:14:42 +0100

    fix export encoding

--- influenzanet/management/commands/survey_export.py
+++ influenzanet/management/commands/survey_export.py
@@ -10,29 +10,29 @@ from django.core.management.base import BaseCommand
 from apps.pollster import models
 from django.template import loader
 import datetime
+import io
 
 class Command(BaseCommand):
     help = 'Export xml definitions for intake & weekly'
-   
+
     option_list = BaseCommand.option_list + (
     )
-      
+
     def handle(self, *args, **options):
         now = datetime.datetime.now()
         file_id = format(now, '%Y-%m-%d-%H%M')
-        
+
         self.export_survey("weekly", file_id)
         self.export_survey('intake', file_id)
-        
+
     def export_survey(self, shortname, file_id):
-        
-        survey = models.Survey.objects.get(shortname=shortname) 
+
+        survey = models.Survey.objects.get(shortname=shortname)
         print "Found Survey %s %d "  % (shortname, survey.id)
-        
+
         xml = loader.render_to_string('pollster/survey_export.xml', { "survey": survey }, context_instance=None)
-       
+
         fn = shortname + '_' + file_id + '.xml'
-        f = open(fn, 'w')
+        f = io.open(fn, 'w', encoding='utf-8')
         f.write(xml)
         f.close()
-        
